51784414944a538b2c30d8821aed2b184c3939b5
Grigory Sapogov <grisha.sapgv@mail.ru>
2023-11-15 17:00:34 +0500

    Initial Commit

---
+++ Composition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Composition
+//
+//  Created by Grigory Sapogov on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
